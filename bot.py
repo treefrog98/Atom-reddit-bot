62ff436e1d65a02aaf8a31e584d8ea1815c54025
Sriram Rameshbabu <35275218+treefrog98@users.noreply.github.com>
2018-08-25 01:07:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,9 @@
-
-
 import praw
 
-# uses constructor from the praw class
 bot = praw.reddit(user_agent = 'bot', client_id = '', client_secret='', username = '', password = '')
 
-# go to specific subreddit
 subreddit = bot.subreddit('science')
 
-#use stream
 words = subreddit.stream.comments()
 
 for word in words:
